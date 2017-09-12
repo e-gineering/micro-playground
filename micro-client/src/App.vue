948650fc3b97fad1bdac3c6ce409d4d36344e1f9
Nathan Erwin <nderwin@users.noreply.github.com>
2017-09-12 15:06:18 -0400

    - rounded out the login/logout lifecycle - added a navbar with the logout button - trap the token and store it in local storage

--- micro-client/src/App.vue
+++ micro-client/src/App.vue
@@ -1,18 +1,55 @@
 <template>
 	<div id="app">
-		<img src="./assets/logo.png">
-		<router-view></router-view>
-		<modal v-if="showModal">
+		<modal v-if="!authenticated">
 			<h3 slot="header" class="modal-title">
-				Modal title
+				Please sign in
 			</h3>
 
+			<div slot="body">
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input type="text" id="username" class="form-control" placeholder="Username" autofocus="true" v-model="username" />
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" id="password" class="form-control" placeholder="Password" v-model="password" />
+				</div>
+			</div>
+
 			<div slot="footer">
-				<button type="button" class="btn btn-outline-info" @click="closeModal()">Close </button>
 				<button type="button" class="btn btn-primary" data-dismiss="modal" @click="submitAndClose()">Submit</button>
 			</div>
 		</modal>
-		<button type="button" class="btn btn-primary" @click="openModal()">Open Modal</button>
+
+		<div v-if="authenticated">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<router-link to="/" class="navbar-brand">
+							<img src="./assets/logo.png" width="24" height="24">
+						</router-link>
+					</div>
+
+					<div class="collapse navbar-collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<li>
+								<button class="btn btn-danger log" @click="handleLogout()">Log out</button>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+
+			<div class="container">
+				<ul class="nav nav-tabs">
+					<li role="presentation"><router-link to="/">Home</router-link></li>
+					<li role="presentation"><router-link to="/one">One</router-link></li>
+					<li role="presentation"><router-link to="/two">Two</router-link></li>
+				</ul>
+
+				<router-view></router-view>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -26,29 +63,66 @@
 		},
 		data () {
 			return {
-				showModal: false
+				authenticated: false,
+				username: '',
+				password: ''
 			}
 		},
 		methods: {
-			openModal () {
-				this.showModal = true
-			},
-			closeModal () {
-				this.showModal = false
-			},
 			submitAndClose () {
+				if (this.username && this.password) {
+					var thiz = this
+
+					fetch('http://localhost:8080/security/resources/authentication/login', {
+						method: 'POST',
+						mode: 'cors',
+						headers: new Headers({
+							'Content-Type': 'application/json'
+						}),
+						body: JSON.stringify({
+							username: thiz.username,
+							password: thiz.password
+						})
+					}).then(function (raw) {
+						return raw.json()
+					}).then(function (json) {
+						var token = json.token
+
+						thiz.setToken(token)
+
+						thiz.authenticated = thiz.isAuthenticated()
+						thiz.username = ''
+						thiz.password = ''
+					}).catch(function (err) {
+						console.log(err)
+					})
+				}
+			},
+			handleLogout () {
+				this.clearToken()
+				this.authenticated = this.isAuthenticated()
+			},
+			getToken () {
+				console.log('fetching token')
+				return localStorage.getItem('micro-token')
+			},
+			setToken (token) {
+				localStorage.setItem('micro-token', token)
+			},
+			clearToken () {
+				localStorage.removeItem('micro-token')
+			},
+			isAuthenticated: function () {
+				var rawToken = this.getToken()
+
+				return !!rawToken
 			}
 		}
 	}
 </script>
 
 <style>
-	#app {
-		font-family: 'Avenir', Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
+	.log {
+		margin: 6px 10px 0 0;
 	}
 </style>
