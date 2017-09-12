2ba5e5280ff194d1db5150ea37887dcd3f69c62c
Nathan Erwin <nderwin@users.noreply.github.com>
2017-09-12 10:15:38 -0400

    - added a basic modal dialog box

--- micro-client/src/App.vue
+++ micro-client/src/App.vue
@@ -2,12 +2,43 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
+	<modal v-if="showModal">
+		<h3 slot="header" class="modal-title">
+			Modal title
+		</h3>
+		
+		<div slot="footer">
+			<button type="button" class="btn btn-outline-info" @click="closeModal()">Close </button>
+			<button type="button" class="btn btn-primary" data-dismiss="modal" @click="submitAndClose()">Submit</button>
+		</div>
+	</modal>
+	<button type="button" class="btn btn-primary" @click="openModal()">Open Modal</button>
   </div>
 </template>
 
 <script>
+import Modal from './components/Modal'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Modal
+  },
+  data () {
+    return {
+      showModal: false
+    }
+  },
+  methods: {
+    openModal () {
+      this.showModal = true
+    },
+    closeModal () {
+      this.showModal = false
+    },
+    submitAndClose () {
+    }
+  }
 }
 </script>
 
