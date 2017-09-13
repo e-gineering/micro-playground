ffc17a4dce74d1f71e14b1ff855cf926f8987392
Nathan Erwin <nderwin@users.noreply.github.com>
2017-09-13 08:38:12 -0400

    - updated unit and integration tests to work - updated the project version

--- micro-client/test/e2e/specs/test.js
+++ micro-client/test/e2e/specs/test.js
@@ -2,18 +2,17 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function (browser) {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
-    const devServer = browser.globals.devServerURL
+	'default e2e tests': function (browser) {
+		// automatically uses dev Server port from /config.index.js
+		// default: http://localhost:8080
+		// see nightwatch.conf.js
+		const devServer = browser.globals.devServerURL
 
-    browser
-      .url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end()
-  }
+		browser
+		.url(devServer)
+		.waitForElementVisible('#app', 5000)
+		.assert.elementPresent('.modal')
+		.assert.containsText('h3', 'Please sign in')
+		.end()
+	}
 }
