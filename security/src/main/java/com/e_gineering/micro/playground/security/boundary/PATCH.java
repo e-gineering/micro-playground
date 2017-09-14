14feb5fa8fa76f8a015815619ef016f588a8560d
Nathan Erwin <nathan.d.erwin@gmail.com>
2017-09-14 12:37:43 -0400

    - renamed the packages for the Java code - renamed the group name for the Docker images

---
+++ security/src/main/java/com/e_gineering/micro/playground/security/boundary/PATCH.java
@@ -0,0 +1,16 @@
+package com.e_gineering.micro.playground.security.boundary;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.ws.rs.HttpMethod;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@HttpMethod("PATCH")
+@Documented
+public @interface PATCH {
+    
+}
--- security/src/main/java/com/github/nderwin/micro/playground/security/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.github.nderwin.micro.playground.security;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
